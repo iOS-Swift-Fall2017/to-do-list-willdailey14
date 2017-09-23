fa6ba4f2ddb12748bde27733e12030f3dd5754a4
Will Dailey <willdailey14@macbook-pro-3.local>
2017-09-23 16:59:25 -0400

    Initial Commit

---
+++ To Do List/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  To Do List
+//
+//  Created by Will Dailey on 9/23/17.
+//  Copyright © 2017 Will Dailey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
